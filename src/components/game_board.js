41a4ef42cdb2a98aa6cfb70b8e180647cfe49212
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-26 20:46:43 -0500

    Created light array as a state to carry all lights

--- src/components/game_board.js
+++ src/components/game_board.js
@@ -1,16 +1,40 @@
 import React, {Component} from 'react';
-import Light from './light';
 
-function GameBoard(props) {
-  return (
-    <div className="game-board">
-      <Light />
-      <Light />
-      <Light />
-      <Light />
-      <Light />
-    </div>
-  );
+class GameBoard extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lights: []
+    }
+
+  }
+
+  componentWillMount() {
+    const lights = []
+    for(var i = 0; i < 9; i++) {
+      lights.push({id: i, active: Math.random() > 0.5});
+    }
+    this.setState({lights});
+  }
+
+  renderLight() {
+    return this.state.lights.map((light) => {
+      return (
+        <div  key={light.id} className="light">
+          {light.active ? "on" : " off"}
+        </div> );
+    });
+  }
+
+  render() {
+    return (
+      <div className="game-board">
+        {this.renderLight()}
+      </div>
+    );
+  }
 }
 
 
