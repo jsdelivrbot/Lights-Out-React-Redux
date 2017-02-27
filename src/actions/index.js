c318f33605272e696a33d5de412dc853d4b57208
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-27 11:03:36 -0500

    Added RANDOMIZE_LIGHTS action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
 export const TOGGLE_LIGHT = 'TOGGLE_LIGHT';
+export const RANDOMIZE_LIGHTS = 'RANDOMIZE_LIGHTSS';
 
 function toggleLight(id) {
   return {
     type: TOGGLE_LIGHT,
     id: id
+  }
+}
+
+function() randomizeLights() {
+  return {
+    type: RANDOMIZE_LIGHTS
   }
 }
